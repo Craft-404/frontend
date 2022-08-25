752ba8e4724bd1023648cc32bf84932483adad2d
Tanmay Shah <shahhtanmay@gmail.com>
2022-08-25 18:39:41 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 import CreateEmployee from "./components/CreateEmployee/CreateEmployee";
+import DashBoard from "./components/Dashboard/DashBoard";
 import Login from "./components/Login/Login";
 import "./index.css";
-import { Route, Routes } from "react-router-dom";
-import SideBar from "./components/SideBar/SideBar";
-import DashBoard from "./components/Dashboard/DashBoard";
-import { ProtectedRoute } from "./helpers/ProtectedRoute/ProtectedRoute";
 
 function App() {
   const token = localStorage.getItem("token");
   return (
     <div style={{ height: "100%" }}>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/createemployee" element={<CreateEmployee />} />
         <Route path="/dashboard" element={<DashBoard />} />
